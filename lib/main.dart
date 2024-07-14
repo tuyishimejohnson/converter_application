3d55f040662ae8cc17b54400a5fdcfd18915c22f
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-07-14 14:33:51 +0200

    Added the title of the application.

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,37 @@ void main() {
       appBar: AppBar(
         title: Text("Temperature Converter Application"),
         centerTitle: true,
+        backgroundColor: Colors.green[500],
       ),
       body: Center(
-        child: Text("Select Celcius or Fahrenheit"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Select Celsius or Fahrenheit",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0), // Add some space between the text and buttons
+            ElevatedButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: Text("Button 1"),
+            ),
+            SizedBox(height: 10.0), // Add some space between the buttons
+            ElevatedButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: Text("Button 2"),
+            ),
+          ],
+        ),
       ),
     ),
   ));
 }
+
